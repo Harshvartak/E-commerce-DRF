6159b4c2a4d27b4a46b75091072d70eafe626f96
Harsh Vartak <vartak.harsh@gmail.com>
2019-12-24 04:11:04 +0530

    Started auth and crud simultaneously

---
+++ Product/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+from .models import Cart,Product
+
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Cart
+        fields= ['user','total','updated','products','quantity']
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Product
+        fields=['title','descrip','image','cost','published_date','publisher']
